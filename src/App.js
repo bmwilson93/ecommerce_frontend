f92c3b7786ea64e8585baec6c971cbf5e59846a4
Brian Wilson <bmwilson93@gmail.com>
2024-09-24 04:36:03 -0600

    spacing for readability

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
 
 import './App.css';
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+
       <Routes>
         <Route path='/' element={<div />}/>
       </Routes>
