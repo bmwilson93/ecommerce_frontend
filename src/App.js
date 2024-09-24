bd02331aca597e7907a072206a04f29fea7c9dee
Brian Wilson <bmwilson93@gmail.com>
2024-09-24 04:16:13 -0600

    added the Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path='/' element={<div />}/>
       </Routes>
