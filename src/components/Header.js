c32ddf484a68f06a2e03005a6ef8c085ef82f3d6
Brian Wilson <bmwilson93@gmail.com>
2024-09-24 04:37:02 -0600

    added placeholder text and button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='head-top'>
+        <div>e-Commerce</div>
+        {/* <div></div> */}
+        <div>Log In</div>
+      </div>
+      <div className='cart-button-container'>
+        <button>Cart</button>
+      </div>
+    </header>
   )
 }
 
