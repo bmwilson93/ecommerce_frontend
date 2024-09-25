bfe8e2b97ebf4225bed8b792916fb3424bcea41e
Brian Wilson <bmwilson93@gmail.com>
2024-09-25 02:02:48 -0600

    loads products on page load

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Products = () => {
+  const fetchProducts = async () => {
+    // make a request to backend to get all products in database
+    const result = await fetch('http://localhost:4001/api/products');
+    const data = await result.json();
+    console.log(data);
+
+    setProducts(data);
+  }
+
+  const [products, setProducts] = useState([]);
+
+  // Get all products on page load
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div>Products</div>
+    <div>
+      <ul>
+        {products.map(product => {
+          return (<li key={product.id}>{product.name}</li>)
+        })}
+      </ul>
+
+    </div>
   )
 }
 
