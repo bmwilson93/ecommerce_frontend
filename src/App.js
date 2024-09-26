372f8e23c30e82ec492a47462ba05b3c07e63747
Brian Wilson <bmwilson93@gmail.com>
2024-09-26 06:22:44 -0600

    removed some commented code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 import Header from './components/Header';
 import Products from './pages/Products';
@@ -7,14 +8,20 @@ import Product from './pages/Product';
 import './App.css';
 
 function App() {
+  const [cart, setCart] = useState({
+    "size": 0,
+    "items": []
+  })
+
+  
   return (
     <div className="App">
-      <Header />
+      <Header cart={cart}/>
 
       <Routes>
         <Route path='/' element={<div />}/>
         <Route path='/products' element={<Products />}/>
-        <Route path='/product/:id' element={<Product />}/>
+        <Route path='/product/:id' element={<Product setCart={setCart}/>}/>
       </Routes>
     </div>
   );
