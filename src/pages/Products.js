26ee1d860df3750b63b85c7d5b5099e7ef89c654
Brian Wilson <bmwilson93@gmail.com>
2024-09-25 22:55:48 -0600

    added click to navigate to product page

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ProductCard from '../components/ProductCard'
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Products = () => {
+  const navigate = useNavigate();
+
   const fetchProducts = async () => {
     // make a request to backend to get all products in database
     const result = await fetch('http://localhost:4001/api/products');
@@ -24,7 +27,7 @@ const Products = () => {
       <ul className='product-list'>
         {products.map(product => {
           return (
-          <li key={product.id}>
+          <li key={product.id} onClick={() => navigate(`/product/${product.id}`, {state:{product: product}})}>
             <ProductCard product={product}/>
           </li>
             )
