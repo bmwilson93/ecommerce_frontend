d42a6108c873818d9c8d3eccb485e2d387e6f109
Brian Wilson <bmwilson93@gmail.com>
2024-09-25 22:56:19 -0600

    added product image and details

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Product = () => {
+  const location = useLocation();
+  const product = location.state.product;
+
   return (
-    <div>Product</div>
+    <div className='product-container'>
+      <div><img src={product.product_img} /></div>
+      <div className="product-details">
+        <div>
+          <h2>{product.name}</h2>
+          <p className='product-price'>{product.price}</p>
+          <p>{product.description}</p>
+        </div>
+        <div>
+          <div className="add-cart-btn">
+          <span>Add To Cart </span><button>+</button>
+        </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
