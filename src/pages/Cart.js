9fe9e3f36fab375bf91940f79a5395adb164545e
Brian Wilson <bmwilson93@gmail.com>
2024-09-27 06:23:01 -0600

    added fetchCart function and useEffect to get the cart from the express server on cart page load

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import { clearCart } from '../utils/cartUtils'
+import { clearCart, getCart } from '../utils/cartUtils'
 import { useEffect } from 'react'
 import './Cart.css'
 
 const Cart = ({ cart, setCart }) => {
+  const fetchCart = async () => {
+    const currCart = await getCart();
+    setCart(currCart);
+  }
+
+  useEffect(() => {
+    fetchCart();
+  }, [])
+
   const getCartTotal = () => {
-    let sum = 0;
-    for (let i = 0; i < cart.items.length; i++) {
-      sum += (cart.items[i].product.price * cart.items[i].qty);
+    if (cart.items) {
+      let sum = 0;
+      for (let i = 0; i < cart.items.length; i++) {
+        sum += (cart.items[i].product.price * cart.items[i].qty);
+      }
+      return sum;
     }
-    return sum;
   }
 
   return (
@@ -23,15 +34,18 @@ const Cart = ({ cart, setCart }) => {
             return (
               <li key={item.product.id}>
                 <div className='cart-item-container'>
-                  <div>
+
+                  <div className='cart-item-image'>
                     <img src={item.product.product_img} />
                   </div>
 
-                  <div>
+                  <div className='cart-item-details'>
                     <h2>{item.product.name}</h2>
-                    <p>${item.product.price} x {item.qty}</p>
-                    <p>${item.product.price * item.qty}</p>
+                    <p><span className='price-sign'>$</span><span className='price'>{item.product.price} x {item.qty} </span><div><div>+</div><div>-</div></div></p>
+                    <p><span className='price-sign'>$</span><span className='price'>{item.product.price * item.qty}</span></p>
+                    <button className='remove-item-btn btn-blue'>Remove Item</button>
                   </div>
+
 
                 </div>
               </li>)
@@ -52,13 +66,12 @@ const Cart = ({ cart, setCart }) => {
 
       <div className='cart-details-container'>
         <div>
-          Total: ${getCartTotal()}
+          Total: <span className='price-sign'>$</span><span className='price price-total'>{getCartTotal()}</span>
         </div>
         <div>
           <button>Check Out</button>
         </div>
       </div>
-
 
 
 
