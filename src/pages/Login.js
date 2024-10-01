20ce507b286a9528e9178c684357cdc490061c12
Brian Wilson <bmwilson93@gmail.com>
2024-10-01 03:57:09 -0600

    added state to track the inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import { useState } from 'react';
+import { logout } from '../utils/accountUtils';
+import './Login.css'
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChage = (e) => {
+    setPassword(e.target.value);
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('submit login');
+    let result = await logout();
+    console.log(result);
+  }
+
   return (
-    <div>Login</div>
+    <div className='login-container'>
+      <h2>Log In</h2>
+      <form>
+      <input type='email' value={email} onChange={handleEmailChange} placeholder='Email'/>
+      <input type='password' value={password} onChange={handlePasswordChage} placeholder='Password'/>
+        <button 
+          className='btn-blue'
+          onClick={(e) => {
+            handleSubmit(e)
+          }}
+        >
+          Log In
+        </button>
+      </form>
+    </div>
   )
 }
 
