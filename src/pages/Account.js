93aced846d11d9f095620b38ae32279defc19dc0
Brian Wilson <bmwilson93@gmail.com>
2024-10-02 23:53:47 -0600

    updated to redirect to login if no sessionUser found

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import { useEffect } from "react"
+import { useNavigate } from 'react-router-dom'
 
-const Account = () => {
+const Account = ({ sessionUser }) => {
+  const navigate = useNavigate();
+  // useEffect(() => {}, [])
+
   return (
-    <div>Account</div>
+    // TODO check if sessionUser is null, if not logged in, redirect to the login route,
+    // else render the account page
+    <>
+      {
+        sessionUser ? 
+        <div>
+          <p>First Name</p>
+          <p>{sessionUser.first_name}</p>
+          <p>Last Name</p>
+          <p>{sessionUser.last_name}</p>
+          <p>Email</p>
+          <p>{sessionUser.email}</p>
+        </div> 
+        : navigate('/login')
+      }
+
+    </>
   )
 }
 
