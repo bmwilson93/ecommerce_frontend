5d1b819d6b24ffd659879571177f6a8f24eb3b17
Brian Wilson <bmwilson93@gmail.com>
2024-10-03 01:46:29 -0600

    added button to account page, renders when user is logged in

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 import { getCart } from '../utils/cartUtils'
+import { getDisplayName, logout } from '../utils/accountUtils'
 import 'boxicons'
 
-const Header = ({ cart, setCart }) => {
+const Header = ({ cart, setCart, sessionUser }) => {
   const fetchCart = async () => {
     const currCart = await getCart();
     setCart(currCart);
   }
+
+  // add a function to check if user is logged in, and get display name. 
+  // if logged in, render the display name instead of the login/signup buttons
 
   useEffect(() => {
     fetchCart();
@@ -21,21 +25,34 @@ const Header = ({ cart, setCart }) => {
 
         <div>e-Commerce</div>
         <div><Link to={`products`}>Browse Products</Link></div>
-        {/* <div></div> */}
-        <div className='account-button-container'>
-          <button 
-            className='sign-in-button'
-            onClick={() => navigate('/login')}
-          >
-            Log In
-          </button>
-          <button 
-            className='sign-up-button'
-            onClick={() => navigate('/signup')}
+
+        {/* If there is a user session, render their name, else render the login buttons */}
+        {sessionUser ?
+            
+            <button 
+              className='account-btn'
+              onClick={() => navigate('/account')}
             >
-              Sign Up
+              Hi {sessionUser.first_name}
             </button>
-        </div>
+
+          : <div className='account-button-container'>
+            <button 
+              className='sign-in-button'
+              onClick={() => navigate('/login')}
+            >
+              Log In
+            </button>
+            <button 
+              className='sign-up-button'
+              onClick={() => navigate('/signup')}
+              >
+                Sign Up
+              </button>
+          </div>
+        }
+
+
       </div>
 
       <div className='cart-button-container'>
@@ -43,6 +60,8 @@ const Header = ({ cart, setCart }) => {
         <box-icon color='white' name='cart'></box-icon>
            {cart.size}
         </button>
+
+        <button onClick={() => logout()}>Logout</button>
 
       </div>
     </header>
