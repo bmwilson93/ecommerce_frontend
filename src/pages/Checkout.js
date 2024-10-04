7400f54ab0d182d37b0cb249691e0ccb2f2a8067
Brian Wilson <bmwilson93@gmail.com>
2024-10-03 19:59:39 -0600

    added useEffect to check if logged in an page load

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import { useEffect } from "react"
+import { useNavigate } from 'react-router-dom'
+import { isLoggedIn } from "../utils/accountUtils";
 
 const Checkout = () => {
+  const navigate = useNavigate();
+
+  // Check if logged in, if not redirect to the login page
+  const checkOnReload = async () => {
+    const session = await isLoggedIn();
+    if (!session) navigate('/login');
+  }
+
+  useEffect(() => {
+    checkOnReload();
+  }, [])
+
   return (
     <div>Checkout</div>
   )
