3dfb39b883d50cb73d16d665bdc94fb76e9a17dc
Brian Wilson <bmwilson93@gmail.com>
2024-10-08 05:19:33 -0600

    Added, then moved Strip and Element component code to Checkout.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { isLoggedIn } from './utils/accountUtils';
 
+// Components and Pages
 import Header from './components/Header';
 import Products from './pages/Products';
 import Product from './pages/Product';
@@ -13,7 +14,9 @@ import Checkout from './pages/Checkout';
 
 import './App.css';
 
+
 function App() {
+
   const [cart, setCart] = useState({
     "size": 0,
     "items": []
@@ -32,20 +35,20 @@ function App() {
   }, [])
   
   return (
-    <div className="App">
-      <Header cart={cart} setCart={setCart} sessionUser={sessionUser}/>
+      <div className="App">
+        <Header cart={cart} setCart={setCart} sessionUser={sessionUser}/>
 
-      <Routes>
-        <Route path='/' element={<div />}/>
-        <Route path='/products' element={<Products />}/>
-        <Route path='/product/:id' element={<Product setCart={setCart}/>}/>
-        <Route path='/cart' element={<Cart cart={cart} setCart={setCart}/>} />
-        <Route path='/login' element={<Login sessionUser={sessionUser} setSessionUser={setSessionUser}/>} />
-        <Route path='/signup' element={<Signup sessionUser={sessionUser} setSessionUser={setSessionUser}/>} />
-        <Route path='/account' element={<Account sessionUser={sessionUser} setSessionUser={setSessionUser}/>} />
-        <Route path='/checkout' element={<Checkout />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path='/' element={<div />}/>
+          <Route path='/products' element={<Products />}/>
+          <Route path='/product/:id' element={<Product setCart={setCart}/>}/>
+          <Route path='/cart' element={<Cart cart={cart} setCart={setCart}/>} />
+          <Route path='/login' element={<Login sessionUser={sessionUser} setSessionUser={setSessionUser}/>} />
+          <Route path='/signup' element={<Signup sessionUser={sessionUser} setSessionUser={setSessionUser}/>} />
+          <Route path='/account' element={<Account sessionUser={sessionUser} setSessionUser={setSessionUser}/>} />
+          <Route path='/checkout' element={<Checkout />} />
+        </Routes>
+      </div>
   );
 }
 
