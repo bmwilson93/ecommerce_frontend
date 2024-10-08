06ee2072550f7806f4116806e08af347c2a93355
Brian Wilson <bmwilson93@gmail.com>
2024-10-08 05:17:29 -0600

    created CheckoutForm, moved the form from Checkout.js and added Stripe and PaymentElement code

---
+++ src/components/CheckoutForm.js
@@ -0,0 +1,43 @@
+import { useStripe, useElements, PaymentElement } from "@stripe/react-stripe-js";
+
+const CheckoutForm = () => {
+    // Stipe
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      if (!stripe || !elements) {
+        // Stripe.js hasn't yet loaded
+        //Make sure to disable form submittion until Stripe.js has loaded. 
+        return;
+      }
+  
+      const result = await stripe.confirmPayment({
+        // `Elements` instance that was used to create the Payment Element
+        elements, 
+        confirmParams: {
+          return_url: `${window.location.origin}/complete`,
+        },
+      });
+  
+      if (result.error) {
+        // show the error to customer
+        console.log(result.error.message);
+      } else { // check else if (paymentIntent && paymentIntent.status === 'succeeded')
+        // redirect to the 'return_url' use navigate('/return_url');
+      }
+    };
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Test</h1>
+      <PaymentElement />
+      <button disabled={!stripe}>Submit</button>
+    </form>
+  )
+}
+
+export default CheckoutForm
