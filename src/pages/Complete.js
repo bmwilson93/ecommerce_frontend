ca2ddc43c1b5b03a343902feced25e8adb5f996c
Brian Wilson <bmwilson93@gmail.com>
2024-10-08 05:34:08 -0600

    added stripe code to retrieve PaymentIntent to check for successful payment

--- src/pages/Complete.js
+++ src/pages/Complete.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useStripe } from "@stripe/react-stripe-js"
 
 const Complete = () => {
+  const stripe = useStripe();
+
+
+  useEffect (() => {
+    if (!stripe) {
+      return
+    }
+
+    // Retrieve the 'payment_intent_client_secret" query parameter applied to return_url
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      'payment_intent_client_secret'
+    );
+
+    // Retrieve PaymentIntent
+    stripe
+      .retrievePaymentIntent(clientSecret)
+      .then(({paymentIntent}) => {
+        if (paymentIntent.status === 'succeeded') {
+          // create new order
+          // send info to the server to create the order
+          // then display the returned order info
+        }
+      })
+  }, [stripe]);
+
+
   return (
     <div>Complete</div>
   )
