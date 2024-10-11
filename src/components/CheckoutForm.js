0bd65db12b6b0498ae3e1d4cf69e37a3e139b5fd
Brian Wilson <bmwilson93@gmail.com>
2024-10-11 02:25:06 -0600

    updated the return_url to redirect to /checkout/complete after payment

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -18,7 +18,7 @@ const CheckoutForm = () => {
         // `Elements` instance that was used to create the Payment Element
         elements, 
         confirmParams: {
-          return_url: `${window.location.origin}/complete`,
+          return_url: `${window.location.origin}/checkout/complete`,
         },
       });
   
