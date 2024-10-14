bd5e3449f4ea59832bafe1eb6f5bafaef236f007
Brian Wilson <bmwilson93@gmail.com>
2024-10-14 01:43:58 -0600

    Removed Complete page route, added by to pass cart total to server to create payment intent

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,26 +1,21 @@
 import { useEffect, useState } from "react"
-import { useNavigate, useLocation, Route, Routes } from 'react-router-dom'
+import { useNavigate, useLocation, Route, Routes, Outlet } from 'react-router-dom'
 import { isLoggedIn } from "../utils/accountUtils";
 
+// Stripe Imports
 import {Elements} from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 
+// Components
 import CheckoutForm from "../components/CheckoutForm";
-import Complete from "./Complete";
+
 
 const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
 
+
 const Checkout = () => {
-  const options = {
-    mode: 'payment',
-    amount: 1099,
-    currency: 'usd',
-    appearance: {
-      theme: 'night'
-    }
-  };
   const location = useLocation();
-  const cartTotal = location.state.cartTotal;
+  const cartTotal = location.state ? location.state.cartTotal : 0;
 
   const [clientSecret, setClientSecret] = useState("")
 
@@ -31,18 +26,23 @@ const Checkout = () => {
     const session = await isLoggedIn();
     if (!session) navigate('/login', {state:{nextPage: '/checkout'}});
 
-    // if user is logged in, then get stripe client secret
-    const result = await fetch(`${process.env.REACT_APP_API_PATH}/orders/create-payment-intent`, {
-      method: "POST",
-      body: JSON.stringify({"amount": cartTotal * 100}), // * by 100 to get the total in cents for stripe
-      headers: {
-        "Content-Type": "application/json",
-      },
-      credentials: "include"
-    });
-    const secret = await result.json();
-    console.log(secret.client_secret);
-    setClientSecret(secret.client_secret);
+    console.log(location.pathname);
+    // only generate a paymentIntent if path != /checkout/complete
+    if (location.pathname != '/checkout/complete') {
+      // if user is logged in, then get stripe client secret
+      const result = await fetch(`${process.env.REACT_APP_API_PATH}/orders/create-payment-intent`, {
+        method: "POST",
+        body: JSON.stringify({"amount": cartTotal * 100}), // * by 100 to get the total in cents for stripe
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include"
+      });
+      const secret = await result.json();
+      console.log(`secret.client_secret: ${secret.client_secret}`);
+      setClientSecret(secret.client_secret);
+
+    }
   }
 
   useEffect(() => {
@@ -52,13 +52,10 @@ const Checkout = () => {
   return (
     <div>
       <h1>hi</h1>
-      {console.log(clientSecret)}
+      {console.log(`clientSecret: ${clientSecret}`)}
       {stripePromise && clientSecret && (
         <Elements stripe={stripePromise} options={{ clientSecret }}>
-          <Routes>
-            <Route path='/' element={<CheckoutForm />} />
-            <Route path='/complete' element={<Complete />} />
-          </Routes>
+          <CheckoutForm />
         </Elements>
       )}
     </div>
