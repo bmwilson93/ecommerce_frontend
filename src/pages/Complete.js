7a42a678aa79bba32b60a6cc0a88b4e5fe9c88a4
Brian Wilson <bmwilson93@gmail.com>
2024-10-15 03:52:04 -0600

    added more order info and list of order items

--- src/pages/Complete.js
+++ src/pages/Complete.js
@@ -1,5 +1,7 @@
 import { useLocation, Link } from "react-router-dom";
 
+import './Complete.css';
+
 const Complete = ({ cart, sessionUser }) => {
   const location = useLocation();
   const newOrder = location.state.newOrder;
@@ -7,11 +9,42 @@ const Complete = ({ cart, sessionUser }) => {
   return (
     <div>
       {newOrder.order_number ? (
-        <div>
+        <div className='complete-container'>
           <h1>Thank you for your order!</h1>
 
-          <p>{newOrder.order_number}</p>
-          <p>{newOrder.order_total}</p>
+          <p>Order #<span className="bold ">{newOrder.order_number}</span></p>
+          <p>Total <span className="price">{newOrder.order_total}</span></p>
+
+          <ul>
+            {newOrder.items.items.map(item => (
+              <li className="order-list-li">
+                <div>
+
+                  <div>
+                    <img src={item.product.product_img} alt="product image"/>
+                  </div>
+
+                  <div className="order-item-details">
+                    <p className='order-product-name'>{item.product.name}</p>
+                    {/* <p>qty: {item.qty}</p> */}
+                    {/* <p>${item.product.price.toFixed(2)}</p> */}
+                    <div>
+                      <p>
+                        (<span className='price-sign'>$</span>
+                        <span className=''>{item.product.price.toFixed(2)} x {item.qty} </span>)
+                      </p>
+                      <p className='order-price-line-item'>
+                        <span className='price-sign'></span>
+                        <span className='price cart-price'>${(item.product.price * item.qty).toFixed(2)}</span>
+                      </p>
+
+                    </div>
+                  </div>
+
+                </div>
+              </li>
+            ))}
+          </ul>
 
           <Link to="/products">Continue Shopping</Link>
 
