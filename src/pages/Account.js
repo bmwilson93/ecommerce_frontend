92baad31aebbf9877c586efd056eeaff8f9428a4
Brian Wilson <bmwilson93@gmail.com>
2024-10-15 04:15:11 -0600

    added list to render the orders on the account

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,15 +1,20 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from 'react-router-dom'
 import { logout } from "../utils/accountUtils";
-import { isLoggedIn } from "../utils/accountUtils";
+import { isLoggedIn, getOrders } from "../utils/accountUtils";
 
 const Account = ({ sessionUser, setSessionUser }) => {
   const navigate = useNavigate();
+  const [orders, setOrders] = useState(null)
 
   // Check if logged in, if not redirect to the login page
   const checkOnReload = async () => {
     const session = await isLoggedIn();
     if (!session) navigate('/login');
+
+    // if session, then fetch the accounts orders
+    const orders = await getOrders();
+    setOrders(orders);
   }
 
   const handleLogout = async () => {
@@ -27,7 +32,8 @@ const Account = ({ sessionUser, setSessionUser }) => {
       {
         // Only render the page is there is a session
         sessionUser ? 
-        <div>
+
+        <div className="account-container">
           <p>First Name</p>
           <p>{sessionUser.first_name}</p>
           <p>Last Name</p>
@@ -35,7 +41,30 @@ const Account = ({ sessionUser, setSessionUser }) => {
           <p>Email</p>
           <p>{sessionUser.email}</p>
           <button onClick={() => handleLogout()}>Logout</button>
+
+
+          <div className="account-order-container">
+            <h2>Your Orders</h2>
+            <ul>
+
+              {/* Render a list of orders on the account */}
+              {orders.map(order => (
+                <li>
+                  <div>
+                    {order.order_number}
+                    {order.order_total}
+                    {order.created_at}
+                    {order.items.items.length}
+                  </div>
+                </li>
+              ))}
+
+            </ul>
+          </div>
+
+
         </div> 
+
         : <></>
       }
 
