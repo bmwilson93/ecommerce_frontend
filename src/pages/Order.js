a330af0d334cbffab5da1778209069384a0a1fe3
Brian Wilson <bmwilson93@gmail.com>
2024-11-10 21:57:56 -0700

    updated to display in dollars

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -11,7 +11,7 @@ const Order = () => {
   return (
     <div className='order-container'>
       <h2>Order #<span className="bold ">{order.order_number}</span></h2>
-      <p>Order Total: <span className="price">{(order.order_total / 100).toFixed(2)}</span></p>
+      <p>Order Total: <span className="price">${(order.order_total / 100).toFixed(2)}</span></p>
       <p>Ordered on {order.created_at.substring(0, 10)}</p>
 
       <OrderList items={order.items.items} />
